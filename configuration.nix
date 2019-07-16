812ebd34c29a3bc95e3339ca9c245f282090c846
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-07-16 23:14:20 +0200

    Remove useless comments

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,6 @@
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "connor";
