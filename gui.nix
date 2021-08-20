f0b2e4e2833790d4c30bcfeea056f8669fb644fa
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2021-08-21 01:08:05 +0200

    Realign

--- gui.nix
+++ gui.nix
@@ -1,40 +1,39 @@
 { config, pkgs, callPackage, ... }: 
 {
-    environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw  ???? --- what does this do?? xD
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw  ???? --- what does this do?? xD
 
-    fonts.fonts = with pkgs; [
-        source-code-pro
-        font-awesome-ttf
-        # TODO: fix terminal fonts so that lsd can be used
-    ];
+  fonts.fonts = with pkgs; [
+    source-code-pro
+    font-awesome-ttf
+    # TODO: fix terminal fonts so that lsd can be used
+  ];
 
-    services.xserver = {
-        enable = true;
+  services.xserver = {
+    enable = true;
 
-        autorun = false;
-        displayManager = {
-          startx.enable = true;
-          defaultSession = "none+i3";
-        };
-
-        layout = "pl";
-        xkbOptions = "caps:ctrl_modifier";
-
-        desktopManager = {
-            xterm.enable = false;
-        };
-
-        windowManager.i3 = {
-            enable = true;
-            extraPackages = with pkgs; [
-                feh
-                dmenu
-                i3lock-color
-                i3blocks
-                networkmanager_dmenu
-           ];
-        };
+    autorun = false;
+    displayManager = {
+      startx.enable = true;
+      defaultSession = "none+i3";
     };
+
+    layout = "pl";
+    xkbOptions = "caps:ctrl_modifier";
+
+    desktopManager = {
+        xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        feh
+        i3lock-color
+        i3blocks
+        networkmanager_dmenu
+        ];
+      };
+  };
 
   # fix for Workrave not persisting settings
   services.dbus.packages = with pkgs; [ gnome3.dconf ];
