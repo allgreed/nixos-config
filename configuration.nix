8111723dbf729d585e06d671c1c4a6043a16c5b3
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2022-03-05 13:45:33 +0100

    Add tlp settings

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,15 @@ in
 
   services.tzupdate.enable = true;
   
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0=75;
+      STOP_CHARGE_THRESH_BAT0=80;
+      CPU_SCALING_GOVERNOR_ON_AC="schedutil";
+      DEVICES_TO_DISABLE_ON_STARTUP="bluetooth";
+    };
+  };
 
   console.useXkbConfig = true;
   i18n = {
