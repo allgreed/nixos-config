5a54b19755efe57e4e0d4aa824e1384b7ff1f093
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2023-05-15 00:58:40 +0200

    huh?

--- pkg.nix
+++ pkg.nix
@@ -3,7 +3,8 @@ let
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "3fb8eedc450286d5092e4953118212fa21091b3b"; # 12-04-2022
+    #rev = "3fb8eedc450286d5092e4953118212fa21091b3b"; # 12-04-2023
+    rev = "f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5"; # 27-06-2022
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   };
   # TODO: see if this is necessary after channel bump
