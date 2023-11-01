896467d234acc7ccdfcdc569e4c808c876f7d047
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2023-11-01 15:18:21 +0100

    Document fancy caching switch

--- configuration.nix
+++ configuration.nix
@@ -185,6 +185,9 @@ in
     SystemMaxUse=1G
   '';
 
+  # uncomment to cache all dependencies locally (might download crapload of stuff)
+  #system.includeBuildDependencies = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
