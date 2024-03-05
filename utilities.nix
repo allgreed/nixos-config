c56a49cf70106c0adbdefe6066366394e4ec9bfc
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2024-03-05 17:20:01 +0100

    Add tw-ical, no need to do this manually

--- utilities.nix
+++ utilities.nix
@@ -23,6 +23,8 @@
       # TODO: persistent = true also is a good idea, since I want this to happen every day, especially when the craptop is off
       # TODO: waaaaait, can I just have a service without a seperate timer?
       # https://nixos.wiki/wiki/Systemd/Timers
+      # TODO: waaaaait, can I just have a service without a seperate timer?
+      # https://nixos.wiki/wiki/Systemd/Timers
     };
   };
 
@@ -46,6 +48,32 @@
     timerConfig = {
       OnCalendar = "hourly";
       Unit = "ogar-metric-reporter.service";
+    };
+  };
+
+  config.systemd.services.tw-ical-feed-uploader = {
+    # TODO: actually the processing & upload could happen in a pipe, so that there's no need for writing to disk
+    # TODO: lower service privilage -> read ~/.task;
+    # TODO: and actually -> this could run only and directly after a change is made to a task's sheduled or due attribute or a task with said attribute set
+    enable = true;
+    serviceConfig = {
+      Type = "oneshot";
+      User = "allgreed";
+    };
+    # TODO: is the PATH thing an awfull hack? o.0
+    script = ''
+        export PATH=$PATH:/run/current-system/sw/bin
+        cd ~/Desktop/tw-ical-feed
+        nix-shell --command "make upload"
+    '';
+  };
+  config.systemd.timers.tw-ical-feed-uploader = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "tw-ical-feed-uploader.service" ];
+    timerConfig = {
+      # TODO: did you see the comments above? ;p
+      OnCalendar = "hourly";
+      Unit = "tw-ical-feed-uploader.service";
     };
   };
 }
