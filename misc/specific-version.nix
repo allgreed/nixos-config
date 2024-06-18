1fe4a77c8199f5e687d81c5e4d60f3e900b8e7a7
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2024-06-18 20:14:16 +0000

    Tidy up, keep it just in case

--- misc/specific-version.nix
+++ misc/specific-version.nix
@@ -2,7 +2,7 @@
 # TODO: some channel numbers potentialy? missing, guess this could be more dynamic
 #{ channels ? [ "23.05" "22.11" "21.05" "20.03" "19.09" "19.03" "18.03" "17.03" "16.03" "15.09" "14.12" "14.04" ]
 # I'm low on disk space, don't want to risk garbage collect now, it should be in those two
-{ channels ? [ "22.11" "22.05" "21.11" "21.05" ]
+{ channels ? [ "24.05" "23.05" "22.11" "22.05" "21.11" "21.05" ]
 , attrs ? builtins.attrNames (import <nixpkgs> {})
 , system ? builtins.currentSystem
 , args ? { inherit system; }
